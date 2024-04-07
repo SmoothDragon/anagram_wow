e87c5f8ce0947439d210c532afd53b6c8e14616f
Thomas Draper <tgdrape@gmail.com>
2024-04-07 11:21:01 -0700

    Complete initial letterbank script.

--- src/bin/letterbank.rs
+++ src/bin/letterbank.rs
@@ -1,42 +1,46 @@
 use std::io::{ BufRead, BufReader };
-use std::error::Error;
+use std::cmp::*;
 use clap::Parser;
 use itertools::Itertools;
 
 use anagram_wow::char_set::CharSet;
-use anagram_wow::char_prime::CharPrime;
 
 
-
-/// Search for words that match the letters given
-#[derive(Parser)]
-struct Cli {
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
     /// Letters to use
     letters: String,
+
+    /// Minimum characters of output
+    #[arg(short, long, default_value_t = 0)]
+    min: usize,
+
+    /// Maximum characters of output
+    #[arg(long, default_value_t = 15)]
+    max: usize,
 }
 
+fn main() {
+    let args = Args::parse();
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let args = Cli::parse();
-
-    // let target = CharPrime::new(&args.letters.to_uppercase());
-    let target = CharPrime::from(args.letters.to_uppercase().as_str());
-    let target_set = CharSet::from(args.letters.to_uppercase().as_str());
-    let length = args.letters.len();
+    let required = CharSet::from(args.letters.chars()
+        .filter(|ch| ch.is_ascii_uppercase())
+        .collect::<String>()
+        .as_str()
+        );
+    let allowed = CharSet::from(args.letters.to_uppercase().as_str());
+    let min_length = max(required.len(), args.min);
     const WORDLIST:&str = include_str!("../share/WOW24.txt");
 
     let matches = BufReader::new(WORDLIST.as_bytes())
         .lines()
         .flatten()
-        .filter(|s| {s.len()==length})
-        .filter(|s| {target_set==CharSet::from(s.as_str())})
-        // .filter(|s| {target_set==s.as_str().into()})
-        // .filter(|s| {target_set==s.parse().expect("parse failed")})
-        .filter(|s| {target==CharPrime::from(s.as_str())})
+        .filter(|s| (min_length..=args.max).contains(&s.len()))
+        .filter(|s| CharSet::from(s.as_str()).contains(&required))
+        .filter(|s| allowed.contains(&CharSet::from(s.as_str())))
         .join("\n")
         ;
 
     println!("{}", matches);
-    println!("letterbank");
-    Ok(())
 }
