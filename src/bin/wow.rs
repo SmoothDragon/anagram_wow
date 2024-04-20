cbb7708c34efb53c1f21fbbd563c19ca80b84339
Thomas Draper <tgdrape@gmail.com>
2024-04-20 10:34:14 -0700

    Add Query struct.

--- src/bin/wow.rs
+++ src/bin/wow.rs
@@ -1,5 +1,6 @@
 use std::io::{ BufRead, BufReader };
 use std::fmt::Display;
+use core::cmp::max;
 use clap::*;
 use itertools::Itertools;
 
@@ -14,6 +15,43 @@ struct Args {
     #[clap(value_enum)]
     method: Method,
     letters: String,
+}
+
+struct Query {
+    required_set: CharSet,
+    // required_multiset: CharMultiSet,
+    allowed_set: CharSet,
+    // lenth_min: usize,
+    // lenth_max: usize,
+    blanks: usize,
+}
+
+impl From<&str> for Query {
+    /// Parse an alphanumeric string
+    /// Uppercase letters are required
+    /// Lowercase letters are optional
+    /// Number represents # of blanks allowed
+    fn from(word: &str) -> Self {
+        // if !word.chars().all(char::alphanumeric) { return None };
+        let blanks = word.chars()
+            .filter(|ch| ch.is_ascii_digit())
+            .map(|ch| ch.to_digit(10).unwrap() as usize)
+            .fold(0, |acc, d| 10*acc+d)
+            ;
+        let required = word.chars()
+            .filter(|ch| ch.is_ascii_uppercase())
+            .join("")
+            ;
+        let allowed = word.to_uppercase().chars()
+            .filter(|ch| ch.is_ascii_alphabetic())
+            .join("")
+            ;
+        Self{
+            required_set: CharSet::from(&*required),
+            allowed_set: CharSet::from(&*allowed),
+            blanks: blanks,
+        }
+    }
 }
 
 #[derive(clap::ValueEnum, Clone)]
@@ -46,11 +84,30 @@ fn anahook(letters: &str, seq: impl IntoIterator<Item=impl AsRef<str>+Display>) 
         .filter(move |s| CharMultiSet::from(&*s.as_ref()).contains(target))
 }
 
+fn letterbank(letters: &str, seq: impl IntoIterator<Item=impl AsRef<str>+Display>) -> impl Iterator<Item=impl AsRef<str>+Display> {
+    // TODO import these
+    let arg_min = 2;
+    let arg_max = 15;
+
+    let required = CharSet::from(letters.chars()
+        .filter(|ch| ch.is_ascii_uppercase())
+        .collect::<String>()
+        .as_str()
+        );
+    let allowed = CharSet::from(letters.to_uppercase().as_str());
+    let min_length = max(required.len(), arg_min);
+    seq.into_iter()
+        .filter(move |s| (min_length..=arg_max).contains(&(s.as_ref().len())))
+        .filter(move |s| CharSet::from(&*s.as_ref()).contains(required))
+        .filter(move |s| allowed.contains(CharSet::from(&*s.as_ref())))
+}
+
 
 
 fn main() {
     let args = Args::parse();
 
+    // TODO: make a Query struct
     // If numbers are separated, they will be joined into a multidigit number.
     let blanks = args.letters.chars()
         .filter(|ch| ch.is_ascii_digit())
@@ -69,6 +126,7 @@ fn main() {
     let ss = match args.method {
         Method::anagram => anagram(&letters, word_list).join("\n"),
         Method::anahook => anahook(&letters, word_list).join("\n"),
+        Method::letterbank => letterbank(&args.letters, word_list).join("\n"),
         _ => "Undefined".to_string(),
     };
     println!("{}", ss);
